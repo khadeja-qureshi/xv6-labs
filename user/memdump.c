18a7c9d8c4b6759a11ff7a9e22e323382d0e4efa
khadeja-q <khadejaqureshi10@gmail.com>
2025-09-23 20:49:00 +0500

    Implement memdump for Problem Set 1

--- user/memdump.c
+++ user/memdump.c
@@ -60,6 +60,59 @@ main(int argc, char *argv[])
 void
 memdump(char *fmt, char *data)
 {
-  // Your code here.
+
+  char *p = data;
+
+  for (int i = 0; fmt[i] != '\0'; i++) {
+    char f = fmt[i];
+
+    switch (f) {
+    case 'i': {
+      int val = *(int*)p;
+      printf("%d\n", val);
+      p += 4;
+      break;
+    }
+
+    case 'p': {
+      uint64 val = *(uint64*)p;
+      printf("%lx\n", val);
+      p += 8;
+      break;
+    }
+
+    case 'h': {
+      short val = *(short*)p;
+      printf("%d\n", val);
+      p += 2;
+      break;
+    }
+
+    case 'c': {
+      char val = *p;
+      printf("%c\n", val);
+      p += 1;
+      break;
+    }
+
+    case 's': {
+      char *str = *(char**)p;
+      printf("%s\n", str);
+      p += 8;
+      break;
+    }
+
+    case 'S': {
+      printf("%s\n", p);
+      p += strlen(p) + 1;
+      break;
+    }
+
+    default:
+      printf("Unknown format: %c\n", f);
+      break;
+     }
+   }
+
 
 }
